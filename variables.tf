654bfc6597ec2c662b8caf06d549c5383b857f5b
Jonas Maratita <jonas.maratita@gmail.com>
2023-11-08 06:51:15 +0000

    #23 bucket should no longer use random provider

--- variables.tf
+++ variables.tf
@@ -6,3 +6,13 @@ variable "user_uuid" {
     error_message = "User UUID must be in the format of UUID (e.g., 123e4567-e89b-12d3-a456-426655440000)"
   }
 }
+
+variable "bucket_name" {
+  description = "AWS S3 Bucket Name"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name)) && !can(regex("^[.-]|[-.]|[.]{2,}", var.bucket_name))
+    error_message = "S3 bucket name must be between 3 and 63 characters, use only lowercase letters, numbers, hyphens, and periods. It cannot start or end with a period, and cannot have consecutive periods, hyphens, or a period at the beginning or end."
+  }
+}
