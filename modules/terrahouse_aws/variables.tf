bb64e43435eacfdc065b2c93b8e5c152661a1118
Jonas Maratita <jonas.maratita@gmail.com>
2023-11-09 21:45:14 +0000

    #31 only upload index.html if a content version has changed

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -36,3 +36,13 @@ variable "error_html_filepath" {
     error_message = "The specified path for error.html does not exist."
   }
 }
+
+variable "content_version" {
+  description = "The content version should be a positive integer starting at 1"
+  type        = number
+
+  validation {
+    condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
+    error_message = "The content_version must be a positive integer starting at 1"
+  }
+}
